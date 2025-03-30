87c7f6de6dc578a5beeb0bb2e423fff3a47df7a9
Marcel <marceli.durazinski@gmail.com>
2025-03-30 14:02:26 +0200

    Added Product view and link to this page

---
+++ Script/Products_view.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const productDefinitions = [
+    {
+      productId: "P001",
+      name: "Laptop Dell Inspiron 15",
+      category: "Elektronika",
+      price: 2999.99,
+      stock: 10,
+      status: "Dostępny",
+    },
+    {
+      productId: "P002",
+      name: "Apple iPhone 15",
+      category: "Telefony",
+      price: 3399.00,
+      stock: 5,
+      status: "Dostępny",
+    },
+    {
+      productId: "P003",
+      name: "Konsola Sony PlayStation 5 Pro",
+      category: "Gaming",
+      price: 3499.00,
+      stock: 3,
+      status: "Oczekuje na dostawę",
+    }
+  ];
+  
+});
